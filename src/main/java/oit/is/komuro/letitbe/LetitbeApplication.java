e9d62078017b4b038bad37aa6cdf1f5565501c79
NaRHa0713 <e1b19104@oit.ac.jp>
2021-10-26 16:40:24 +0900

    sample5-6の実装

--- src/main/java/oit/is/komuro/letitbe/LetitbeApplication.java
+++ src/main/java/oit/is/komuro/letitbe/LetitbeApplication.java
@@ -2,8 +2,10 @@ package oit.is.komuro.letitbe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class LetitbeApplication {
 
 	public static void main(String[] args) {

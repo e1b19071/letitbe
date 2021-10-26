46dd0a15a1e4ce23b69a682f9e7659770ba35abb
e1b19071 <e1b19071@oit.ac.jp>
2021-10-26 15:01:09 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/komuro/letitbe/LetitbeApplication.java
@@ -0,0 +1,13 @@
+package oit.is.komuro.letitbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetitbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetitbeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/komuro/letitbe/LetitbeApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.komuro.letitbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetitbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
